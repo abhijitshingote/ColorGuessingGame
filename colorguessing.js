7a24a5086c4b12ecaa4899e894035df6da2b270d
abhijitshingote <abhijitshingote@gmail.com>
2018-05-24 22:26:58 -0400

    nothing new

--- colorguessing.js
+++ colorguessing.js
@@ -1,30 +1,47 @@
-var choice1color='rgb('+getRandomInt(0,255).toString()+','+getRandomInt(0,255).toString() + 
-',' + getRandomInt(0,255).toString() + ')'
-
 var choices= document.querySelectorAll('.choices');
-choices.forEach(function(choice) {
-	choice.style.background = 'rgb('+getRandomInt(0,255).toString()+','+getRandomInt(0,255).toString() + 
-',' + getRandomInt(0,255).toString() + ')'
-})
-
-// choice1.style.background = choice1color;
+var question=document.querySelector('#question');
+var newgame=document.querySelector('.newgame');
 
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+	  return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
+function initialize_game(){
+
+	choices.forEach(function(choice) {
+		choice.style.background = 'rgb('+getRandomInt(0,255).toString()+','+getRandomInt(0,255).toString() + 
+	',' + getRandomInt(0,255).toString() + ')';
+		choice.classList.remove('incorrectanswer');
+		choice.classList.add('choices');
+	})
+	question.textContent=choices[getRandomInt(0,5)].style.background.toUpperCase();
+	}
+
+newgame.addEventListener('click', function(){
+	initialize_game();
+})
+
+function correct_answer() {
+		alert('Correct Answer');
+		initialize_game();
+}
+function incorrect_answer(o) {
+		o.classList.add('incorrectanswer');
+		o.style.background='rgba(255,255,255)';
 }
 
-var question=document.querySelector('#question')
-question.textContent=choices[getRandomInt(0,5)].style.background
+initialize_game();
 
 choices.forEach(function(choice) {
 	choice.addEventListener('click', function(){
-		if(question.textContent===this.style.background){
-			alert('Correct Answer')
+
+		if(question.textContent===this.style.background.toUpperCase()){
+			correct_answer();
+			console.log('correctanswer');
 		}
 		else {
-			this.style.background='rgb(255,255,255)';
-			this.style.border='0px';
-			this.textContent= '';
+			incorrect_answer(this);
+
 		}
 	})
 })
